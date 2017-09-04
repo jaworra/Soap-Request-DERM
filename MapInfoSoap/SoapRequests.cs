94317c76d84f6af37c46291f043d0d72315783b0
jaworra <john.a.worrall@gmail.com>
2017-09-04 15:22:48 +1000

    https://github.com/jaworra/SoapRequestDERM_Plus.git

--- MapInfoSoap/SoapRequests.cs
+++ MapInfoSoap/SoapRequests.cs
@@ -10,7 +10,19 @@ using System.Xml.Linq;
 using System.Web;
 using System.Net;
 
+using System.Security;
+using System.Collections;
+
+
 using System.Windows.Forms;
+
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
+
+using System.Linq;
+
+
 
 namespace MapInfoSoap
 {
@@ -19,7 +31,9 @@ namespace MapInfoSoap
         // Takes address information and returns records that match the criteria best
         public static string ValidateAddressSoapRequest(string unitNumber, string buildingName, string numberFirst, string numberSuffixFirst, string numberLast, string numberSuffixLast, string roadName, string roadNameType, string roadNameSuffix, string localityName, string localGovernmentName, string stateName)
         {
-            var uri = new Uri("https://information.qld.gov.au/service/land/property/ValidationService/1/soap");
+            //var uri = new Uri("https://information.qld.gov.au/service/land/property/ValidationService/1/soap");
+            var uri = new Uri("https://information.qld.gov.au/service/Addressing/ValidationService/PLSplus/soap");
+
 
             HttpWebRequest req = (HttpWebRequest)WebRequest.CreateDefault(uri);
             req.ContentType = "text/xml;charset=\"utf-8\"";
@@ -58,72 +72,73 @@ namespace MapInfoSoap
         // Takes a single line address and returns all matching addresses in SOAP XML
         public static string ParseValidAddress(string address)
         {
+
+
+            string username = "plspQ_SAMSinfo";
+            string password = "SEce37f1c";
+                                                
             // Construct request
-            var uri = new Uri("https://information.qld.gov.au/service/land/property/ValidationService/1/soap");
+            var uri = new Uri("https://information.qld.gov.au/service/Addressing/ValidationService/PLSplusQG/soap"); //endpoint
 
-            HttpWebRequest req = (HttpWebRequest)WebRequest.CreateDefault(uri);
-            req.ContentType = "text/xml;charset=\"utf-8\"";
-            req.Method = "POST";
-            req.Accept = "text/xml";
-            req.Headers.Add("SOAPAction", @"information.derm.qld.gov.au/service/Addressing/ValidationService/1/soap/IValidationService/ParseValidAddress");
+            SecurityBindingElement securityElement = SecurityBindingElement.CreateUserNameOverTransportBindingElement();
+            securityElement.IncludeTimestamp = false;
+            var encodingElement = new TextMessageEncodingBindingElement(MessageVersion.Soap11, Encoding.UTF8);
+            var transportElement = new HttpsTransportBindingElement();
+            var binding = new CustomBinding(securityElement, encodingElement, transportElement);
+            var endpoint = new EndpointAddress("https://information.qld.gov.au/service/Addressing/ValidationService/PLSplusQG/soap");
+            //var endpoint = new EndpointAddress(uri);
 
-            // Create XML body for the request
-            XmlDocument soapEnv = new XmlDocument();
-            soapEnv.LoadXml(@"<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:soap='information.derm.qld.gov.au/service/Addressing/ValidationService/1/soap'><soapenv:Header/><soapenv:Body><soap:ParseValidAddress><soap:addressString>" + address + "</soap:addressString></soap:ParseValidAddress></soapenv:Body></soapenv:Envelope>");
+            var service = new ServiceReference1.PLSpQGClient(binding, endpoint);
+            service.ClientCredentials.UserName.UserName = username;
+            service.ClientCredentials.UserName.Password = password;
 
-            using (Stream stream = req.GetRequestStream())
-            {
-                soapEnv.Save(stream);
-            }
+          
+            MapInfoSoap.ServiceReference1.ParseAddressRequest obj  = new ServiceReference1.ParseAddressRequest();
+            obj.AddressString = address;
+            //obj.MeshblockOption = "@TRUE";
 
-            // Begin the request
-            IAsyncResult asyncRes = req.BeginGetResponse(null, null);
-
-            // Wait for the response
-            asyncRes.AsyncWaitHandle.WaitOne();
-
-            // Read the response
-            string soapResponse;
-
+            string responseStr = null; 
             try
             {
-                using (WebResponse webResponse = req.EndGetResponse(asyncRes))
-                {
-                    using (StreamReader rd = new StreamReader(webResponse.GetResponseStream()))
+
+            var resultsX = service.ParseAddress(obj);
+
+
+                //Build string here
+                
+                int LoopCnt = resultsX.Results.Length;
+                if (LoopCnt >= 1)
                     {
-                        soapResponse = rd.ReadToEnd();
+                    LoopCnt = 1; //Only return the first value
+                    for (int i = 0; i < LoopCnt; i++)
+                        {
+                         responseStr += resultsX.Results[i].Address.RoadNumber.First.ToString();
+                         responseStr += " " + resultsX.Results[i].Address.Road.Name.ToString();
+                         responseStr += " " + resultsX.Results[i].Address.Road.TypeCode.ToString();
+                         responseStr += " " + resultsX.Results[i].Address.Locality.ToString();
+                         responseStr += "|" + resultsX.Results[i].Geocode[i].Latitude.ToString();
+                         responseStr += "|" + resultsX.Results[i].Geocode[i].Longitude.ToString() + ";";
+
+                         if (resultsX.Results[i].Confidence < 80) //If there record isn't cofident
+                             {
+                                 responseStr = "Error incorrect value";
+                             }
+                        }
                     }
-                }
-
-                XDocument xD = XDocument.Load(XmlReader.Create(new StringReader(soapResponse)));
-
-                IEnumerable<XElement> parcels =
-                    (from e in xD.Root.Descendants()
-                     where e.Name.LocalName == "Parcel"
-                     select e);
-
-                List<SoapAddress> addresses = new List<SoapAddress>();
-                foreach (XElement p in parcels)
+                else
                 {
-                    SoapAddress sA = new SoapAddress(p);
-                    addresses.Add(sA);
+                    responseStr = "Error incorrect value";
                 }
 
-                // Sort and remove duplicates
-                string[] result = (from adress in addresses.OrderBy(a => a.Number).ToList() select adress.ToString()).ToArray();
-                var newstring = new HashSet<string>(result);
-                soapResponse = string.Concat(newstring);
+
             }
-            catch(WebException e) //bad web request error
+            catch (WebException e) //bad web request error
             {
-                soapResponse = e.Message;
-            }
-            catch(Exception e) //bad request error
-            {
-                soapResponse = e.Message;
+                responseStr = "Too Many Results";
             }
 
-            return soapResponse;
+
+            return responseStr;
 
         }
 
